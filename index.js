18411785635ab787484747fd04b483518c1ff943
Sravanthi931 <ksravs917@gmail.com>
2025-06-02 14:43:36 +0530

    .

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-import express from 'express'
-import cors from 'cors'
-const app=express()
-app.listen(8080,()=>{
-    console.log("Server Started");
+import express from "express";
+import cors from "cors";
+const app = express();
+app.listen(8080, () => {
+  console.log("Server Started");
 });
-app.use(cors())
-app.get("/",(req,res)=>{
-    return res.send("Good Morning");
+app.use(cors());
+app.get("/", (req, res) => {
+  return res.send("Good Morning");
 });
-app.get("/greet",(req,res)=>(res.send("greetings")))
-app.get("/name",(req,res)=>(res.send("Sravanthi")))
-app.get("/weather",(req,res)=>(res.send("30 degrees")))
+
+app.get("/greet", (req, res) => {
+  res.send("Greetings");
+});
+
+app.get("/name", (req, res) => {
+  res.send("Sravanthi");
+});
+
+app.get("/weather", (req, res) => {
+  res.send("30 degrees");
+});
+
+app.get("/products", (req, res) => {
+  const products = [
+    { name: "Product 1", price: 34 },
+    { name: "Product 2", price: 64 },
+    { name: "Product 3", price: 45 },
+  ];
+  res.json(products);
+});
+
