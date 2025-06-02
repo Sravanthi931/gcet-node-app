79e1c52d4233864aa41a114b9a4ad24b61772b16
Sravanthi931 <ksravs917@gmail.com>
2025-06-02 14:28:07 +0530

    .

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 const app=express()
 app.listen(8080,()=>{
     console.log("Server Started");
 });
+app.use(cors())
 app.get("/",(req,res)=>{
     return res.send("Good Morning");
 });
