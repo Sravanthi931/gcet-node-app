99662d43dbb9cc2e3e3a51bad3457f7566554704
Sravanthi931 <ksravs917@gmail.com>
2025-06-03 19:58:56 +0530

    u

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
-
 app.use("/products", productRouter);
+
 
 app.listen(8080, () => {
   mongoose.connect("mongodb://localhost:27017/gcet");
