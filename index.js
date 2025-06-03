d399cc5604dd574e11de0c919caac5537d62627a
Sravanthi931 <ksravs917@gmail.com>
2025-06-03 14:55:11 +0530

    up

--- index.js
+++ index.js
@@ -2,48 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import userRouter from "./routes/userRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+
 const app = express();
 app.use(cors());
+app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/gcet").then(() => {
-  console.log("MongoDB connected");
-  app.listen(8080, () => {
-    console.log("Server started on http://localhost:8080");
-  });
-});
+app.use("/users", userRouter);
 
-const userSchema = new mongoose.Schema({
-  name: { type: String },
-});
+app.use("/products", productRouter);
 
-const user = mongoose.model("User", userSchema); // lowercase `user` here
-
-app.get("/", (req, res) => {
-  res.send("Good Morning");
-});
-
-app.get("/register", async (req, res) => {
-  const result = await user.create({ name: "Sravanthi" }); // also lowercase here
-  res.json(result);
-});
-
-app.get("/greet", (req, res) => {
-  res.send("Greetings");
-});
-
-app.get("/name", (req, res) => {
-  res.send("Sravanthi");
-});
-
-app.get("/weather", (req, res) => {
-  res.send("30 degrees");
-});
-
-app.get("/products", (req, res) => {
-  const products = [
-    { name: "Product 1", price: 34 },
-    { name: "Product 2", price: 64 },
-    { name: "Product 3", price: 45 },
-  ];
-  res.json(products);
+app.listen(8080, () => {
+  mongoose.connect("mongodb://localhost:27017/gcet");
+  console.log("Server Started");
 });
