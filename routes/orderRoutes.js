a292c61044049b324b1a0e83a3e2a151287fa2be
Sravanthi931 <ksravs917@gmail.com>
2025-06-08 19:17:27 +0530

    s

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -5,8 +5,19 @@ const orderRouter = express.Router();
 
 orderRouter.post("/new", async (req, res) => {
   const { email, orderValue } = req.body;
-  const result = await orderModel.insertOne({ email, orderValue });
+  const result = await orderModel.create({ email, orderValue });
   return res.json(result);
 });
 
-export default orderRouter
+orderRouter.get("/:id", async (req, res) => {
+  const email = req.params.id;
+  const result = await orderModel.find({ email });
+  return res.json(result);
+});
+
+orderRouter.get("/all", async (req, res) => {
+  const result = await orderModel.find();
+  return res.json(result);
+});
+
+export default orderRouter;
